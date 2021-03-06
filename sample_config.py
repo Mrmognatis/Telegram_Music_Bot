64b58d5cced0edcfcade332ca95dff26a4fdccfa
only-music <79091681+only-music@users.noreply.github.com>
2021-03-07 00:03:47 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,4 +1,4 @@
-bot_token = "1488727769:AAElRU7yS309GP1t54F635ASDF"
+bot_token = "1644868562:AAEfDQuY4PqcDwQtP900lFOdY2L1sal2iwk"
 radio_link = "http://peridot.streamguys.com:7150/Mirchi"
-sudo_chat_id = -1001485876964
-owner_id = 1243703097
+sudo_chat_id = -1001461928414
+owner_id = 1254831277
